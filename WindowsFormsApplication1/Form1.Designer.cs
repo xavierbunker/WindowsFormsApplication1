96cf7ad0a7b84f1126c77fad5e73e8036ce149e9
Avery Bunker <avery.bunker@me.com>
2016-07-04 19:00:45 -0400

    Updated Form to kill Explorer upon form Initialize and upon Form Closing start explorer

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -93,6 +93,7 @@
             this.Name = "Form1";
             this.Text = "Form1";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,11 +17,27 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
+            try
+            {
+                foreach (Process p in Process.GetProcesses())
+                {
+                    try
+                    {
+                        Process[] exp = Process.GetProcessesByName("explorer");
+                        foreach (Process explorer in exp)
+                            explorer.Kill();
+                    }
+                    catch { }
+                }
+            }
+            catch { }
+
         }
+
 
         private void progressBar1_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -27,14 +45,15 @@ namespace WindowsFormsApplication1
             Rectangle resolution = Screen.PrimaryScreen.Bounds;
             label1.Text = "My Screen Resolution = " + resolution.Size;
             label1.Location = new Point(resolution.Right - 250, 30);
-            
-            monthCalendar1.Location = new Point(resolution.Right-250, 30); 
-                
-                }
+
+            monthCalendar1.Location = new Point(resolution.Right - 250, 30);
+        }
+
+       
 
         private void trackBar1_Scroll(object sender, EventArgs e)
         {
-            
+
             System.Windows.Forms.Form.ActiveForm.Opacity = ((double)(trackBar1.Value) / 100.0);
         }
 
@@ -46,12 +65,17 @@ namespace WindowsFormsApplication1
             if (result == DialogResult.OK) // Test result.
             {
                 string file = openFileDialog1.FileName;
-                
+
             }
             Console.WriteLine(size); // <-- Shows file size in debugging mode.
             Console.WriteLine(result); // <-- For debugging use.
 
             this.BackgroundImage = new Bitmap(openFileDialog1.FileName);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Process.Start(Path.Combine(Environment.GetEnvironmentVariable("windir"), "explorer.exe"));
         }
     }
 }
